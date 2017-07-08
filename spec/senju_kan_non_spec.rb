f007bfb9f9d19aeaa4f4e8e71e814fce125342f4
hanahiroAze <hiroyuki.hanai@gmail.com>
2017-07-08 21:20:15 +0900

    add 3params spec

--- spec/senju_kan_non_spec.rb
+++ spec/senju_kan_non_spec.rb
@@ -33,5 +33,30 @@ RSpec.describe SenjuKanNon do
         expect(subject.count).to eq(9)
       end
     end
+
+    context "valid 3_params" do
+      let(:issai) do
+        { first: [1,2,3], second: ["a","b","c"], third: ["A","B","C"] }
+      end
+
+      it "return Array" do
+        expect(subject.kind_of?(Array)).to eq(true)
+      end
+
+      it "return pairwised Array" do
+        expect(subject.count).to eq(11)
+      end
+      # 1,b,A
+      # 2,c,A
+      # 1,a,A
+      # 3,a,C
+      # 2,a,B
+      # 1,b,C
+      # 1,c,B
+      # 2,b,B
+      # 2,c,C
+      # 3,c,A
+      # 3,b,B
+    end
   end
 end
