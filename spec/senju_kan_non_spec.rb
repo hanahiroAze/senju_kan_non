ba9758eec612bce73201474e5b4773d5fcfd20e8
hanahiroAze <hiroyuki.hanai@gmail.com>
2017-10-01 17:05:53 +0900

    add returning existing file spec

--- spec/senju_kan_non_spec.rb
+++ spec/senju_kan_non_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe SenjuKanNon do
 
   describe "#redeem" do
     subject do
-      SenjuKanNon.redeem(issai)
+      SenjuKanNon.redeem(issai, time)
     end
+
+    let(:time) { nil }
 
     context "invalid params" do
       let(:issai) do
@@ -128,6 +130,27 @@ RSpec.describe SenjuKanNon do
       it "output file" do
         SenjuKanNon.config.file_output = true
         expect{ subject }.to change{ Dir.glob("#{SenjuKanNon.config.file_output_path}*").count }.by(1)
+      end
+    end
+
+    context "enable file use" do
+      let(:issai) do
+        { first: [1,2,3,4],
+          second: ["a"],
+          third: ["b"],
+          forth: ["c"]
+        }
+      end
+      let(:time) { Time.now }
+
+      before do
+        allow(File).to receive(:exist?).and_return(true)
+        allow(File).to receive(:open).and_return(File.open("spec/fixture/history_file_fixture.txt"))
+      end
+
+      it "return file content" do
+        SenjuKanNon.config.use_file = true
+        expect(subject.count).to eq(3)
       end
     end
   end
